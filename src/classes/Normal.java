1cc67352bb6fb088ed86c0c0716245257347a8db
cunhaluisg5 <cunhaluisg5@gmail.com>
2018-11-13 11:48:24 -0200

    Criada classe Normal

---
+++ src/classes/Normal.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+import interfaces.INormal;
+
+/**
+ *
+ * @author Luis
+ */
+public class Normal extends Pokemon implements INormal{
+
+    public Normal() {
+    }
+
+    public Normal(int codigo, String nome, double forca, double ataque, double defesa, double agilidade, EFase fase) {
+        super(codigo, nome, forca, ataque, defesa, agilidade, fase);
+    }
+
+    @Override
+    public String ataqueNormal() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
