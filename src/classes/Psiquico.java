d6b7aa426e1dd8fedead1813c2809b6c36544b24
cunhaluisg5 <cunhaluisg5@gmail.com>
2018-11-13 11:49:32 -0200

    Criada classe Psiquico

---
+++ src/classes/Psiquico.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+import interfaces.IPsiquico;
+
+/**
+ *
+ * @author Luis
+ */
+public class Psiquico extends Pokemon implements IPsiquico{
+
+    public Psiquico() {
+    }
+
+    public Psiquico(int codigo, String nome, double forca, double ataque, double defesa, double agilidade, EFase fase) {
+        super(codigo, nome, forca, ataque, defesa, agilidade, fase);
+    }
+
+    @Override
+    public String ataquePsiquico() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
