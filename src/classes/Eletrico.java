a0cc6d6e3bf8d0424b598ee276deb06ecc2d7a6c
cunhaluisg5 <cunhaluisg5@gmail.com>
2018-11-13 11:44:24 -0200

    Criada classe Eletrico

---
+++ src/classes/Eletrico.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+import interfaces.IEletrico;
+
+/**
+ *
+ * @author Luis
+ */
+public class Eletrico extends Pokemon implements IEletrico{
+
+    public Eletrico() {
+    }
+
+    public Eletrico(int codigo, String nome, double forca, double ataque, double defesa, double agilidade, EFase fase) {
+        super(codigo, nome, forca, ataque, defesa, agilidade, fase);
+    }
+
+    @Override
+    public String ataqueEletrico() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
